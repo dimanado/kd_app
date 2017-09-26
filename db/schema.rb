bf0dc18cef45a77c186ea62e5a838d3c0e077437
ViktorPresniatski <presniatski@gmail.com>
2017-09-26 15:10:44 +0300

    remove session and registration controllers

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,12 @@ ActiveRecord::Schema.define(version: 20170922112432) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "companies", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -32,11 +38,6 @@ ActiveRecord::Schema.define(version: 20170922112432) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    
-  create_table "companies", force: :cascade do |t|
-    t.string "title", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end

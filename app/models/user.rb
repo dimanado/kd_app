e57884f5dca5f979342ba9faad825354a9c18b35
ViktorPresniatski <presniatski@gmail.com>
2017-09-28 15:53:41 +0300

    authorization required for profile, fix bug with create profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
-  
+
   has_one :profile, dependent: :destroy
 
-  after_save :init_profile
+  after_create :create_profile
 
   private
 
-  def init_profile
+  def create_profile
     Profile.create(user_id: id)
   end
 end

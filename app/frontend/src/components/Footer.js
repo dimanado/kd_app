b6d87559c353a1c317f872248bb7bf4d89186aec
Egor <egormakovich@rambler.ru>
2017-09-28 13:53:38 +0300

    Add header and footer (#5)

---
+++ app/frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
+
+class Header extends Component {
+  render() {
+    return (
+      <Navbar fixedBottom>
+      </Navbar>
+    );
+  }
+}
+
+export default Header;
---
+++ app/frontend/src/routes.js
@@ -0,0 +1,27 @@
+import Base from 'App';
+import HomePage from 'HomePage';
+// import LoginPage from 'LoginPage';
+// import SignUpPage from 'SignUpPage';
+
+
+const routes = {
+  component: Base,
+  childRoutes: [
+    {
+      path: '/',
+      component: HomePage
+    }
+
+    // {
+    //   path: '/login',
+    //   component: LoginPage
+    // },
+    //
+    // {
+    //   path: '/signup',
+    //   component: SignUpPage
+    // }
+  ]
+};
+
+export default routes;

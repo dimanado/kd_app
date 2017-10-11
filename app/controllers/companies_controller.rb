c6d320394fee9aecd9ba351106d939eed7aeadd1
mariya <rikka1128@gmail.com>
2017-10-11 13:50:51 +0300

    Delete unused callbacks

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,9 +1,4 @@
 class CompaniesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_company, only: [:show, :update, :destroy]
-  before_action :set_company_representative, only: [:update]
-  before_action :check_user, only: [:update, :destroy]
-
   def index
     @companies = Company.all
   end
